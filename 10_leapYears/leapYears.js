24f900adaa3396b1f66fcba93ef36351530ee67a
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 16:17:35 +0200

    Add year parameter to function() to check if a given year is a leap year.
    Add yearCanBeDividedBy4 to check if a year
    can be divided by 4.
    
    Add century check to ensure leap year.
    
    Add yearCanBeDividedBy400 to check if a year
    can be divided by 400.
    
    Add IF to check divisibility by 4, 100, and 400,
    returning true or false.
    
    Add example output.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const yearCanBeDividedBy4 = year % 4 === 0;
+  const century = year % 100 === 0;
+  const yearCanBeDividedBy400 = year % 400 === 0;
 
+  if (
+    yearCanBeDividedBy4 && (!century || yearCanBeDividedBy400)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+//Examples:
+// leapYears(2000) should return true
+// leapYears(1985) is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
