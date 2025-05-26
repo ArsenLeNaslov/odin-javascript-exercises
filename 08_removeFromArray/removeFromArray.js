1714e423c2b167236aa795ef0e7844bd4779d6fc
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 15:56:18 +0200

    Pass "arr" and ""...values" to the function() to flexibly remove items from the array.
    Add Filter to go through items from the array.
    Add !values.includes(item) to check if item is
    not in the values array.
    
    If item is not in values, it is kept in the new
    array; otherwise, it is removed.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+  return arr.filter(item => !values.includes(item));
 };
+
+// Example
+console.log(removeFromArray([1, 2, 3, 4], 3, 4)); // Output: [1, 2]
 
 // Do not edit below this line
 module.exports = removeFromArray;
