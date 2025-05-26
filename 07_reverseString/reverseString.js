8cb6669491cbe609be79980e7da2981445bfcfc5
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 15:43:50 +0200

    Add Split() to convert Str in single chars. Add Reverse() to reverse the array of chars. Add Join() to convert the array of characters back into a Str. Added example output.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+// Example usage:
+console.log(reverseString('interesting today!')) // !yadot gnitseretni
 
 // Do not edit below this line
 module.exports = reverseString;
