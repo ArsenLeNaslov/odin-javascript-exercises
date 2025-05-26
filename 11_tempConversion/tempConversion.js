cc05dd0e3da78da8ef66e546dabfc8e9c19948dc
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 16:27:35 +0200

    Add Fahrenheit to convertToCelsius. Calculations are based on Form:(F - 32)*5/9 = C
    Add Celsius to convertToFahrenheit.
    Calculations are based on Form:(C * 9/5) + 32 = F
    
    Add Math.round() to both to round results to
    1 decimal place.
    
    Add example outputs for both.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  // Convert Fahrenheit to Celsius
+  // Formula: (F - 32) * 5/9 = C
+  return Math.round(((Fahrenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// Examples
+const convertToCelsiusExample = function() {
+  console.log(convertToCelsius(32)); // Should print 0
+  console.log(convertToCelsius(100)); // Should print 37.8
+  console.log(convertToCelsius(-100)); // Should print -73.3
+  console.log(convertToCelsius(212)); // Should print 100
+};
+
+const convertToFahrenheit = function(Celsius) {
+  // Convert Celsius to Fahrenheit
+  // Formula: (C * 9/5) + 32 = F
+  return Math.round(((Celsius * (9 / 5)) + 32) * 10) / 10;
+};
+
+// Examples
+const convertToFahrenheitExample = function() {
+  console.log(convertToFahrenheit(0)); // Should print 32
+  console.log(convertToFahrenheit(100)); // Should print 212
+  console.log(convertToFahrenheit(-10)); // Should print 14
+  console.log(convertToFahrenheit(37.8)); // Should print 100
 };
 
 // Do not edit below this line
