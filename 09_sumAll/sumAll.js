9534e1c5d18db57e4582110fc778fe89d95a6e39
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 16:09:25 +0200

    Add minimum & maximum parameters to sumAll() it calculates the sum of all integers between 2 given numbers, inclusive.
    Add a For loop to iterate from the minimum
    to the maximum value and accumulates the total.
    
    Add example output.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(minimum, maximum) {
+  let sum = 0;
+  for (let i = minimum; i <= maximum; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+// Example: sumAll(1, 4) should return 10 (1 + 2 + 3 + 4)
 
 // Do not edit below this line
 module.exports = sumAll;
