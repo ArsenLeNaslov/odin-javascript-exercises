6766d8fb63baf13fc58067f375ed0f6639a20324
Arsén <136006631+ArsenLeNaslov@users.noreply.github.com>
2025-05-26 15:32:11 +0200

    const repeatString = function(words, num) {     if (num < 0) return 'ERROR';     let string = '';     for (let i = 0; i < num; i++) {         string += words;     }     return string; };
    // Example usage:
    console.log(repeatString('hey', 3)); // Output: heyheyhey

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(words, num) {
+    if (num < 0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i < num; i++) {
+        string += words;
+    }
+    return string;
 };
+
+// Example usage:
+console.log(repeatString('hey', 3)); // Output: heyheyhey
 
 // Do not edit below this line
 module.exports = repeatString;
