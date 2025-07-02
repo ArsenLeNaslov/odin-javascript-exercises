534a3d02a302e868cf834ac2e205730db2e02aa8
Arsén <rhos.andrea@gmail.com>
2025-07-02 16:42:52 +0200

    Add const fibonacci, which returns the nth Fibonacci number, where n is the input argument

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+// Returns the nth Fibonacci number, where n is the input argument
+const fibonacci = function(countArgu) {
+    // Ensure the argument is a number for calculation
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArgu);
+    } else {
+        count = countArgu;
+    }
 
-};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
