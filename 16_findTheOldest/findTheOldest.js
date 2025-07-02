c112481c06dd7598066a80904e7352f2c4082e5b
Arsén <rhos.andrea@gmail.com>
2025-07-02 17:01:13 +0200

    Add const findTheOldest to calculate the age - given a birth year (nativitas) - and an optional death year (mors). - if the death year is not provided,   it uses the current year.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+// Calculates the age given a birth year (nativitas) and an optional death year (mors).
+// If the death year is not provided, it uses the current year.
+const findTheOldest = function(nativitas, mors) {
+    if (!mors) {
+        mors = new Date().getFullYear();
+    }
 
+    return mors - nativitas;
 };
 
 // Do not edit below this line
