70a167236543032fcc2d1fade99c4ea2b63e47d7
Arsén <rhos.andrea@gmail.com>
2025-07-02 16:30:29 +0200

    Add const palindromes, which considers only letters and numbers, variable contains all valid characters

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,5 @@
-const palindromes = function () {
-
+// Considers only Letters and Numbers, variable contains all valid characters
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 };
 
-// Do not edit below this line
-module.exports = palindromes;
