b0f5eb3b8d65cc61cc1fb8c59ae2144cb4103d41
Arsén <rhos.andrea@gmail.com>
2025-07-02 16:31:23 +0200

    Add const cleanedString, which converts the input string to lowercase, splits it into an array of individual characters, filters out only valid alphanumeric characters, and then rejoins them into a new string.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -3,3 +3,9 @@ const palindromes = function (string) {
   const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 };
 
+// Converts to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+const cleanedString = string
+  .toLowerCase()
+  .split('')
+  .filter(char => alphanumerical.includes(char))
+  .join('');
