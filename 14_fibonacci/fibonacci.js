3c2edf343eb02743fa78be91602df588e6ef98e7
Arsén <rhos.andrea@gmail.com>
2025-07-02 16:50:57 +0200

    Add minor adjustments to comments

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 // Returns the nth Fibonacci number, where N is the input argument
 const fibonacci = function(countArgu) {
 
-// Ensure the argument is a number for calculation
+// Ensures the argument is a number for calculation
     let count;
     if (typeof countArgu !== 'number') {
         count = parseInt(countArgu);
@@ -9,24 +9,24 @@ const fibonacci = function(countArgu) {
         count = countArgu;
     }
 
-// Handle negative input by returning error string
+// Handles negative input by returning error string
     if (count < 0) return "OOPS";
 
 // The 0th Fibonacci number is 0
     if (count == 0) return 0;
 
-// Initialise the first 2 Fibonacci numbers
+// Initialises the first 2 Fibonacci numbers
     let firstPrev = 1;
     let secondPrev = 0;
 
-// Calculate Fibonacci numbers iteratively up to the Nth
+// Calculates Fibonacci numbers iteratively up to the Nth
     for (let i = 2; i <= count; i++) {
         let current = firstPrev + secondPrev;
         secondPrev = firstPrev;
         firstPrev = current;
     }
 
-// Return the nth Fibonacci number
+// Returns the nth Fibonacci number
     return firstPrev;
 };
 
--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,4 +1,6 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    // Uses map to extract the title property from each book object
+    return array.map(book => book.title);   
 
 };
 
