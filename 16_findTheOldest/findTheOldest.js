24697fcf74670da0ad066a167507df7c5b5ba176
Arsén <rhos.andrea@gmail.com>
2025-07-02 17:15:01 +0200

    Add const findTheOldest to find and return the oldest person from an array of Populus objects

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-// Calculates the age given a birth year (nativitas) and an optional death year (mors).
-// If the death year is not provided, it uses the current year.
+// Calculates the age given a birth year (nativitas) and an optional death year (mors)
+// If the death year is not provided, it uses the current year
 const findTheOldest = function(nativitas, mors) {
     if (!mors) {
         mors = new Date().getFullYear();
@@ -8,5 +8,18 @@ const findTheOldest = function(nativitas, mors) {
     return mors - nativitas;
 };
 
+// Finds and returns the oldest person from an array of Populus objects
+const findTheOldest = function(populus) {
+    return populus.reduce((vetustissimus, praesensPersona) => {
+        const vetustissimaAetas = accipeAetas(vetustissima.annusNatalis, vetustissima.annusMortis);
+        const praesensAetas  = accipeAetas(
+            praesensPersona.annusNatalis,
+            praesensPersona.annusMorti
+        );
+
+        return vetustissimaAetas < praesensAetas ? praesensPersona : vetustissima;
+    });
+    };
+    
 // Do not edit below this line
 module.exports = findTheOldest;
